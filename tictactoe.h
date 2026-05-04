c50342837ec0c22e067d91d16e1966b7559c379e
ShravyaPanchagiri <shravy112@gmail.com>
2026-05-04 11:48:56 -0500

    Final cleanup: Source code and Makefile only

--- tictactoe.h
+++ tictactoe.h
@@ -1,6 +1,10 @@
 #ifndef TICTACTOE_H
 #define TICTACTOE_H
 
+//Necessary kernel headers for synchronization primitives
+#include <linux/spinlock.h>
+#include <linux/wait.h>
+
 #define BOARD_SIZE 9
 
 typedef enum{
@@ -14,6 +18,13 @@ typedef struct{
 	cell_t turn;
 	int game_over;
 	cell_t winner;
+
+	//Concurrency Primitive: Spinlock protects the data above from race conditions.
+	spinlock_t lock;
+
+	//Communication Primitive: Wait queue for blocking/unblocking players for turn enforcement.
+	wait_queue_head_t wq;
+
 }ttt_game_t;
 
 #endif
